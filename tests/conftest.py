a715c8f07ef13ab29cd9cb811981a539123783b3
Jon Betts <jon.betts@hypothes.is>
2020-11-18 14:49:44 +0000

    Add client code and tests

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import functools
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock  # Python 2.7 compatibility
+
+import pytest
+
+
+def _autopatcher(request, target, **kwargs):
+    """Patch and cleanup automatically. Wraps :py:func:`mock.patch`."""
+    options = {"autospec": True}
+    options.update(kwargs)
+    patcher = mock.patch(target, **options)
+    obj = patcher.start()
+    request.addfinalizer(patcher.stop)
+    return obj
+
+
+@pytest.fixture
+def patch(request):
+    return functools.partial(_autopatcher, request)

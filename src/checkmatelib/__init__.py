74c49492873f3e4b1c62439ce11fe6e28672f048
Jon Betts <jon.betts@hypothes.is>
2022-04-21 18:28:30 +0100

    Export the `BadURL` now it's pretty important to catch it

--- src/checkmatelib/__init__.py
+++ src/checkmatelib/__init__.py
@@ -1,4 +1,4 @@
 """A client for the Checkmate URL testing service."""
 
 from checkmatelib.client import CheckmateClient
-from checkmatelib.exceptions import CheckmateException
+from checkmatelib.exceptions import BadURL, CheckmateException
